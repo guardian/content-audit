9b58f955c6167331bd62541ff36353b03a86bce9
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2025-04-03 20:13:28 +0100

    Add ECR repo and lambda to CDK

--- cdk/lib/content-audit.ts
+++ cdk/lib/content-audit.ts
@@ -1,15 +1,22 @@
-import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
-import { GuStack } from "@guardian/cdk/lib/constructs/core";
-import type { App } from "aws-cdk-lib";
-import { DockerImageCode, DockerImageFunction } from "aws-cdk-lib/aws-lambda";
+import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
+import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import { Duration, type App } from 'aws-cdk-lib';
+import { Repository } from 'aws-cdk-lib/aws-ecr';
+import { DockerImageCode, DockerImageFunction } from 'aws-cdk-lib/aws-lambda';
 
 export class ContentAudit extends GuStack {
-  constructor(scope: App, id: string, props: GuStackProps) {
-    super(scope, id, props);
+	constructor(scope: App, id: string, props: GuStackProps) {
+		super(scope, id, props);
 
-    new DockerImageFunction(this, 'DockerTutorialFunction', {
-      code: DockerImageCode.fromImageAsset("../../projects/playwright-runner"),
-      functionName: 'playwright-runner',
-    });
-  }
+		const EcrRepo = new Repository(this, 'content-audit-ecr-repo', {
+			repositoryName: 'content-audit/playwright-runner',
+		});
+
+		new DockerImageFunction(this, 'DockerTutorialFunction', {
+			code: DockerImageCode.fromEcr(EcrRepo),
+			functionName: 'playwright-runner',
+			memorySize: 4096,
+			timeout: Duration.seconds(60),
+		});
+	}
 }
