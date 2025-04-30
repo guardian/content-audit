b3b9efa4c6c223236236dd14c43f307f472a311b
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2025-04-30 16:57:23 +0100

    Add temporary code to verify DB works in tests

--- projects/playwright-runner/src/handler.ts
+++ projects/playwright-runner/src/handler.ts
@@ -7,10 +7,22 @@ import { PrismaClient } from "@prisma/client";
  * Create the lambda handler. Dependencies are injected for testing purposes.
  */
 export const createHandler =
-  (auditPage: (url: string) => Promise<void>, prismaClient: PrismaClient): Handler<APIGatewayProxyEvent> =>
+  (
+    auditPage: (url: string) => Promise<void>,
+    prismaClient: PrismaClient
+  ): Handler<APIGatewayProxyEvent> =>
   async (event) => {
     console.log("`playwright-runner` started");
     console.log(`Event payload received: ${event.body}`);
+
+    // Temporary until we add working DB code to test the connection
+    try {
+      const count = await prismaClient.audit_page_run.count();
+      console.log(`Current run count: ${count} (should be 0!)`);
+    } catch (e) {
+      console.error(e);
+      return errorResponse(500, e);
+    }
 
     try {
       const eventJson = JSON.parse(event.body ?? "");
