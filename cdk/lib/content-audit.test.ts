484c9b9af23f5241177848273c5e3315acdb5e86
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2025-05-01 12:50:13 +0100

    Add DB proxy to enable lambda to reuse connections

--- cdk/lib/content-audit.test.ts
+++ cdk/lib/content-audit.test.ts
@@ -5,7 +5,7 @@ import { ContentAudit } from "./content-audit";
 describe("The ContentAudit stack", () => {
   it("matches the snapshot", () => {
     const app = new App();
-    const stack = new ContentAudit(app, "ContentAudit", { stack: "content-api", stage: "TEST" });
+    const stack = new ContentAudit(app, "ContentAudit", { app: 'content-audit', stack: "content-api", stage: "TEST" });
     const template = Template.fromStack(stack);
     expect(template.toJSON()).toMatchSnapshot();
   });
