f41d133df903913da23c895bc7d49cb9170eb232
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2025-05-01 17:10:30 +0100

    Modify local docker configuration to correctly access DB

--- projects/playwright-runner/scripts/setup.sh
+++ projects/playwright-runner/scripts/setup.sh
@@ -1,2 +1,10 @@
 # Add the connection string to the .env file read by Prisma for local development
-echo 'DATABASE_URL="postgresql://content-audit:content-audit@localhost:5432/content-audit?schema=public"' > .env
+LOCAL_TEST_HOSTNAME=localhost
+LOCAL_DOCKER_HOSTNAME=host.docker.internal
+
+write_env() {
+    echo "DATABASE_URL=postgresql://content-audit:content-audit@$1:5432/content-audit?schema=public" > "$2"
+}
+
+write_env $LOCAL_TEST_HOSTNAME ".env"
+write_env $LOCAL_DOCKER_HOSTNAME ".env-docker"
