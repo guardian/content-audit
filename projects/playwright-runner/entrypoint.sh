e00861851d96502c99d135ab726afbf7cb132127
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2025-11-08 23:15:39 +0000

    Minor tidy of entrypoint script

--- projects/playwright-runner/entrypoint.sh
+++ projects/playwright-runner/entrypoint.sh
@@ -2,13 +2,13 @@
 
 export NODE_OPTIONS="--experimental-strip-types --experimental-transform-types"
 
-printf "Entrypoint with handler: $1"
+printf "Entrypoint with handler: $1\n"
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
   NODE_VERSION=$(node -v)
   printf "Running in emulator on node $NODE_VERSION\n"
 
-  exec /usr/local/bin/aws-lambda-rie /usr/bin/npx aws-lambda-ric $1
+  exec /usr/local/bin/aws-lambda-rie /usr/bin/npx aws-lambda-ric "$1"
 else
   printf "Running in lambda on node $NODE_VERSION\n"
-  exec /usr/bin/npx aws-lambda-ric $1
+  exec /usr/bin/npx aws-lambda-ric "$1"
 fi
