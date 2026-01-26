5b5ad4f95e46a0cca291e244be7705eff1fedcfa
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2026-01-26 16:07:18 +0000

    Write in-flight, success, and failure states to DB

--- projects/page-runner/src/handler.ts
+++ projects/page-runner/src/handler.ts
@@ -9,20 +9,12 @@ import { type PrismaClient } from "../prisma/client/client.ts";
 export const createHandler =
   (
     auditPage: (url: string) => Promise<void>,
-    prismaClient: PrismaClient
+    prismaClient: PrismaClient,
   ): Handler<APIGatewayProxyEvent> =>
   async (event) => {
     console.log("`page-runner` started");
     console.log(`Event payload received: ${event.body}`);
-
-    // Temporary until we add working DB code to test the connection
-    try {
-      const count = await prismaClient.audit_page_run.count();
-      console.log(`Current run count: ${count} (should be 0!)`);
-    } catch (e) {
-      console.error(e);
-      return errorResponse(500, e);
-    }
+    let runId: number | undefined;
 
     try {
       const eventJson = JSON.parse(event.body ?? "");
@@ -30,10 +22,32 @@ export const createHandler =
 
       try {
         console.log(`Running page at ${url}`);
+
+        const pageRun = await prismaClient.audit_page_run.create({
+          data: { url },
+        });
+        runId = pageRun.id;
+
         await auditPage(url);
+
+        await prismaClient.audit_page_run.update({
+          where: { id: pageRun.id },
+          data: { status: "COMPLETED" },
+        });
 
         return okResponse("Run complete");
       } catch (e) {
+        if (runId) {
+          await prismaClient.audit_page_run.update({
+            where: { id: runId },
+            data: { status: "FAILED" },
+          });
+        } else {
+          console.log(
+            `Unable to record failure for audit with url ${url}: there was no PENDING record to update`,
+          );
+        }
+
         return errorResponse(500, e);
       }
     } catch (e) {
